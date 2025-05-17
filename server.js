65ca87bdcbc79a784391aa76a662f26289475213
Melvintt <melvint180618@gmail.com>
2025-05-17 19:05:21 +0700

    tambah

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 
-
 const app = express();
 const PORT = 3000;
 const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
 app.get("/", (req, res) => {
-    res.send("It works!")
-})
+    res.send("It works!");
+});
 
 app.get("/search", async (req, res) => {
     const movie = req.query.movie;
@@ -19,21 +18,24 @@ app.get("/search", async (req, res) => {
     try {
         const response = await axios.get(`http://www.omdbapi.com/?s=${movie}&apikey=${OMDB_API_KEY}`);
         res.status(200).json(response.data);
-    } catch (err) 
-    {
+    } catch (err) {
         res.status(404).json({ error: "Movie is not found" });
     }
-})
+});
 
 // curl -X "GET" "http://localhost:3000/details?movieId=tt1517268"
-app.get("details", async (req, res) => {
+app.get("/details", async (req, res) => {
     const movieId = req.query.movieId;
     if (!movieId) 
         return res.status(400).json({ error: "Movie ID is required" });
 
-    // diisi dan dilanjutkan untuk rquests ke detail movie
-
-})
+    try {
+        const response = await axios.get(`http://www.omdbapi.com/?i=${movieId}&apikey=${OMDB_API_KEY}`);
+        res.status(200).json(response.data);
+    } catch (err) {
+        res.status(404).json({ error: "Movie details not found" });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
